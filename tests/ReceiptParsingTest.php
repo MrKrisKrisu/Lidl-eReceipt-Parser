b252e4720bac546a307b767359fb44a2b23d9e75
Kris <git@k118.de>
2020-12-05 19:39:56 +0100

    Added support for PHP 8, Added Workflows and simple test

--- tests/ReceiptParsingTest.php
+++ tests/ReceiptParsingTest.php
@@ -1,8 +1,17 @@
 <?php declare(strict_types=1);
 
+use LidlParser\Parser;
 use PHPUnit\Framework\TestCase;
 
 final class ReceiptParsingTest extends TestCase
 {
+    public function test_parsing()
+    {
+        $receipt = Parser::parse(dirname(__FILE__) . '/receipts/general.jpg');
 
+        $this->assertEquals(1.04, $receipt->getTotal());
+        $this->assertEquals("Thu Aug 13 2020 13:41:00 GMT+0200", $receipt->getTimestamp()->toString());
+        $this->assertEquals("Kreditkarte", $receipt->getPaymentMethod());
+        $this->assertCount(1, $receipt->getPositions());
+    }
 }
