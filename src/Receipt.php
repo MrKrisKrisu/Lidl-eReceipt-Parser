67e144bc0099f1fdd0a026ebd374b6a16c7e1da6
Kristian Stöckel <git@k118.de>
2022-05-27 19:44:48 +0200

    :sparkles: Add receipt id to parser

--- src/Receipt.php
+++ src/Receipt.php
@@ -8,8 +8,7 @@ use LidlParser\Exception\ReceiptParseException;
 use thiagoalessio\TesseractOCR\TesseractOCR;
 use thiagoalessio\TesseractOCR\TesseractOcrException;
 
-class Receipt
-{
+class Receipt {
     private $rawReceipt;
     private $explodedReceipt;
 
@@ -17,21 +16,29 @@ class Receipt
      * @param string $imagePath
      * @throws TesseractOcrException
      */
-    function __construct(string $imagePath)
-    {
-        $ocr = new TesseractOCR($imagePath);
-        $this->rawReceipt = $ocr->run();
-        $this->rawReceipt = str_replace('@', '0', $this->rawReceipt); //Maybe there is a better solution to handle these ocr problem?
+    public function __construct(string $imagePath) {
+        $ocr                   = new TesseractOCR($imagePath);
+        $this->rawReceipt      = $ocr->run();
+        $this->rawReceipt      = str_replace('@', '0', $this->rawReceipt); //Maybe there is a better solution to handle these ocr problem?
         $this->explodedReceipt = explode("\n", $this->rawReceipt);
+    }
+
+    /**
+     * @throws ReceiptParseException
+     */
+    public function getID(): int {
+        if(preg_match('/(\d+) (\d+)\/(\d+) (\d{2}).(\d{2})/', $this->rawReceipt, $match)) {
+            return (int)$match[1];
+        }
+        throw new ReceiptParseException();
     }
 
     /**
      * @return float
      * @throws ReceiptParseException
      */
-    public function getTotal(): float
-    {
-        if (preg_match('/zu zahlen (-?\d+,\d{2})/', $this->rawReceipt, $match))
+    public function getTotal(): float {
+        if(preg_match('/zu zahlen (-?\d+,\d{2})/', $this->rawReceipt, $match))
             return (float)str_replace(',', '.', $match[1]);
         throw new ReceiptParseException();
     }
@@ -40,15 +47,14 @@ class Receipt
      * @return string
      * @throws ReceiptParseException
      */
-    public function getPaymentMethod(): string
-    {
+    public function getPaymentMethod(): string {
         $next = false;
-        foreach ($this->explodedReceipt as $row)
-            if ($next) {
-                if (!preg_match("/(.*) \d+,\d{2}/", $row, $match))
+        foreach($this->explodedReceipt as $row)
+            if($next) {
+                if(!preg_match("/(.*) \d+,\d{2}/", $row, $match))
                     throw new ReceiptParseException();
                 return $match[1];
-            } else if (substr(trim($row), 0, 9) == "zu zahlen")
+            } else if(substr(trim($row), 0, 9) == "zu zahlen")
                 $next = true;
         throw new ReceiptParseException();
     }
@@ -56,8 +62,7 @@ class Receipt
     /**
      * @return bool
      */
-    public function hasPayedCashless(): bool
-    {
+    public function hasPayedCashless(): bool {
         return preg_match('/(Kreditkarte|Karte)/', $this->rawReceipt);
     }
 
@@ -65,10 +70,10 @@ class Receipt
      * @return Carbon
      * @throws ReceiptParseException
      */
-    public function getTimestamp(): Carbon
-    {
-        if (preg_match('/(\d{2}).(\d{2}).(\d{2}) (\d{2}):(\d{2})/', $this->rawReceipt, $match))
+    public function getTimestamp(): Carbon {
+        if(preg_match('/(\d{2}).(\d{2}).(\d{2}) (\d{2}):(\d{2})/', $this->rawReceipt, $match)) {
             return Carbon::create("20" . $match[3], $match[2], $match[1], $match[4], $match[5], 0, 'Europe/Berlin');
+        }
         throw new ReceiptParseException();
     }
 
@@ -76,10 +81,9 @@ class Receipt
      * @return int
      * @throws ReceiptParseException
      */
-    private function getProductStartLine(): int
-    {
-        foreach (explode("\n", $this->rawReceipt) as $line => $content)
-            if (trim($content) == "EUR")
+    private function getProductStartLine(): int {
+        foreach(explode("\n", $this->rawReceipt) as $line => $content)
+            if(trim($content) == "EUR")
                 return $line + 1;
         throw new ReceiptParseException();
     }
@@ -88,10 +92,9 @@ class Receipt
      * @return int
      * @throws ReceiptParseException
      */
-    private function getProductEndLine(): int
-    {
-        foreach (explode("\n", $this->rawReceipt) as $line => $content)
-            if (substr(trim($content), 0, 9) == "zu zahlen")
+    private function getProductEndLine(): int {
+        foreach(explode("\n", $this->rawReceipt) as $line => $content)
+            if(substr(trim($content), 0, 9) == "zu zahlen")
                 return $line - 1;
         throw new ReceiptParseException();
     }
@@ -101,10 +104,9 @@ class Receipt
      * @return Position
      * @throws PositionNotFoundException|ReceiptParseException
      */
-    public function getPositionByName(string $name): Position
-    {
-        foreach ($this->getPositions() as $position) {
-            if ($position->getName() == $name)
+    public function getPositionByName(string $name): Position {
+        foreach($this->getPositions() as $position) {
+            if($position->getName() == $name)
                 return $position;
         }
         throw new PositionNotFoundException("Position '$name' not found");
@@ -115,26 +117,25 @@ class Receipt
      * @return array
      * @throws ReceiptParseException
      */
-    public function getPositions(): array
-    {
-        $positions = [];
+    public function getPositions(): array {
+        $positions    = [];
         $lastPosition = NULL;
 
-        for ($lineNr = $this->getProductStartLine(); $lineNr <= $this->getProductEndLine(); $lineNr++) {
+        for($lineNr = $this->getProductStartLine(); $lineNr <= $this->getProductEndLine(); $lineNr++) {
             //echo $this->explodedReceipt[$lineNr];
-            if ($this->isProductLine($lineNr)) {
+            if($this->isProductLine($lineNr)) {
 
-                if ($lastPosition !== NULL) {
-                    $positions[] = $lastPosition;
+                if($lastPosition !== NULL) {
+                    $positions[]  = $lastPosition;
                     $lastPosition = NULL;
                 }
 
-                if (preg_match('/(.*) (-?\d+,\d{2}) ([A-Z])/', $this->explodedReceipt[$lineNr], $match)) {
+                if(preg_match('/(.*) (-?\d+,\d{2}) ([A-Z])/', $this->explodedReceipt[$lineNr], $match)) {
                     $lastPosition = new Position();
                     $lastPosition->setName(trim($match[1]));
                     $lastPosition->setPriceTotal((float)str_replace(',', '.', $match[2]));
                     $lastPosition->setTaxCode($match[3]);
-                } elseif (preg_match('/(.*) (-?\d+,\d{2})/', $this->explodedReceipt[$lineNr], $match)) {
+                } elseif(preg_match('/(.*) (-?\d+,\d{2})/', $this->explodedReceipt[$lineNr], $match)) {
                     $lastPosition = new Position();
                     $lastPosition->setName(trim($match[1]));
                     $lastPosition->setPriceTotal((float)str_replace(',', '.', $match[2]));
@@ -160,29 +161,26 @@ class Receipt
 
         }
 
-        if ($lastPosition !== NULL)
+        if($lastPosition !== NULL)
             $positions[] = $lastPosition;
 
-        if (count($positions) == 0)
+        if(count($positions) == 0)
             throw new ReceiptParseException("Cannot parse any products on receipt");
 
         return $positions;
     }
 
-    private function isWeightLine($lineNr)
-    {
+    private function isWeightLine($lineNr) {
         return false; //TODO: Receipt example needed
         return strpos($this->expl_receipt[$lineNr], 'kg') !== false;
     }
 
-    private function isAmountLine($lineNr)
-    {
+    private function isAmountLine($lineNr) {
         return false; //TODO: Receipt example needed
         return strpos($this->expl_receipt[$lineNr], ' Stk x') !== false;
     }
 
-    private function isProductLine($lineNr)
-    {
+    private function isProductLine($lineNr) {
         return true; //TODO: Receipt example needed
         return !$this->isWeightLine($lineNr) && !$this->isAmountLine($lineNr);
     }
--- tests/ReceiptParsingTest.php
+++ tests/ReceiptParsingTest.php
@@ -3,12 +3,11 @@
 use LidlParser\Parser;
 use PHPUnit\Framework\TestCase;
 
-final class ReceiptParsingTest extends TestCase
-{
-    public function test_parsing()
-    {
-        $receipt = Parser::parse(dirname(__FILE__) . '/receipts/general.jpg');
+final class ReceiptParsingTest extends TestCase {
 
+    public function testParsing(): void {
+        $receipt = Parser::parse(dirname(__DIR__) . '/tests/receipts/general.jpg');
+        $this->assertEquals(7152, $receipt->getID());
         $this->assertEquals(1.04, $receipt->getTotal());
         $this->assertEquals("Thu Aug 13 2020 13:41:00 GMT+0200", $receipt->getTimestamp()->toString());
         $this->assertEquals("Kreditkarte", $receipt->getPaymentMethod());
